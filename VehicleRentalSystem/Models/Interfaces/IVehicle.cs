8a4904fa3f5337d92cfd94fda4a05915fd84bbb6
GalinaTeneva <116523337+GalinaTeneva@users.noreply.github.com>
2024-06-14 13:14:13 +0300

    Fix accidental submodule

---
+++ VehicleRentalSystem/Models/Interfaces/IVehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleRentalSystem.Models.Interfaces
+{
+    internal interface IVehicle
+    {
+        public string VehicleBrand { get; }
+        public string VehicleModel { get; }
+        public decimal VehicleValue { get; }
+    }
+}
---
+++ VehicleRentalSystem/Models/Vehicle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleRentalSystem.Models.Interfaces;
+
+namespace VehicleRentalSystem.Models
+{
+    public class Vehicle : IVehicle
+    {
+        private string vehicleBrand;
+        private string vehicleModel;
+        private decimal vehicleValue;
+
+        public Vehicle(string brand, string model, decimal value)
+        {
+            this.VehicleBrand = brand;
+            this.VehicleModel = model;
+            this.VehicleValue = value;
+        }
+
+        public string VehicleBrand
+        {
+            get { return this.vehicleBrand; }
+            set { this.vehicleBrand = value; }
+        }
+
+        public string VehicleModel
+        {
+            get { return this.vehicleModel; }
+            set { this.vehicleModel = value; }
+        }
+
+        public decimal VehicleValue
+        {
+            get { return this.vehicleValue; }
+            set { this.vehicleValue = value; }
+        }
+    }
+}
